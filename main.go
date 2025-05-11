499ccd36db27c96ad4ebfd35186e39020abfa2de
BrenoCRSilva <brenocrs@gmail.com>
2025-05-11 19:26:42 -0300

    feat: user login logic and password hashing

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/BrenoCRSilva/chirpy/internal/auth"
 	"github.com/BrenoCRSilva/chirpy/internal/database"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -20,40 +22,199 @@ type apiConfig struct {
 	dbQueries      *database.Queries
 }
 
-type User struct {
+type Chirp struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	Email     string    `json:"email"`
+	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+type ChirpRequest struct {
+	Body   string    `json:"body"`
+	UserID uuid.UUID `json:"user_id"`
+}
+
+type User struct {
+	ID             uuid.UUID `json:"id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Email          string    `json:"email"`
+	HashedPassword string    `json:"hashed_password,omitempty"`
 }
 
 type userRequest struct {
-	Email string `json:"email"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
-func (cfg *apiConfig) placeholder(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) loginUser(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	req := userRequest{}
 	err := decoder.Decode(&req)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Error decoding JSON:", err)
+		return
 	}
-	user, err := cfg.dbQueries.CreateUser(r.Context(), req.Email)
+	dbUser, err := cfg.dbQueries.GetUserByEmail(r.Context(), req.Email)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusUnauthorized)
+		log.Println("Error getting user by email:", err)
+		return
 	}
-	data, err := json.Marshal(user)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+	if err := auth.CheckPasswordHash(req.Password, dbUser.HashedPassword); err != nil {
+		log.Println("Error checking password hash:", err)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
-	if _, err := w.Write(data); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+	user := User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	respondWithJSON(w, http.StatusOK, user)
 }
 
-func (cfg *apiConfig) middleware(next http.Handler) http.Handler {
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	req := userRequest{}
+	err := decoder.Decode(&req)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Error decoding JSON:", err)
+		return
+	}
+	hashed, err := auth.HashPassword(req.Password)
+	if err != nil {
+		log.Println("Error hashing password:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	dbUserParams := database.CreateUserParams{
+		Email:          req.Email,
+		HashedPassword: hashed,
+	}
+	dbUser, err := cfg.dbQueries.CreateUser(r.Context(), dbUserParams)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error creating user:", err)
+		return
+	}
+	user := User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+	respondWithJSON(w, http.StatusCreated, user)
+}
+
+func (cfg *apiConfig) getChirps(w http.ResponseWriter, r *http.Request) {
+	chirps := make([]Chirp, 0)
+	dbChirps, err := cfg.dbQueries.GetChirps(r.Context())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error getting chirps:", err)
+	}
+	for _, dbChirp := range dbChirps {
+		chirps = append(chirps, Chirp{
+			ID:        dbChirp.ID,
+			CreatedAt: dbChirp.CreatedAt,
+			UpdatedAt: dbChirp.UpdatedAt,
+			Body:      dbChirp.Body,
+			UserID:    dbChirp.UserID,
+		})
+	}
+	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+func (cfg *apiConfig) getChirp(w http.ResponseWriter, r *http.Request) {
+	idPath := r.PathValue("chirpId")
+	uuidChirp, err := uuid.Parse(idPath)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Error parsing chirp ID:", err)
+	}
+	dbChirp, err := cfg.dbQueries.GetChirp(r.Context(), uuidChirp)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		log.Println("Error getting chirp:", err)
+	}
+	chirp := Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserID:    dbChirp.UserID,
+	}
+	respondWithJSON(w, http.StatusOK, chirp)
+}
+
+func (cfg *apiConfig) createChirp(w http.ResponseWriter, r *http.Request) {
+	type ReturnError struct {
+		Error string `json:"error"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	req := ChirpRequest{}
+	err := decoder.Decode(&req)
+	if err != nil {
+		respondWithJSON(
+			w,
+			http.StatusInternalServerError,
+			ReturnError{Error: "Something went wrong"},
+		)
+		return
+	} else if len(req.Body) > 140 {
+		respondWithJSON(w, http.StatusBadRequest, ReturnError{Error: "Chirp is too long"})
+		return
+	}
+	dbChirpParams := database.CreateChirpParams{
+		Body:   req.Body,
+		UserID: req.UserID,
+	}
+	dbChirp, err := cfg.dbQueries.CreateChirp(r.Context(), dbChirpParams)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error creating chirp:", err)
+		return
+	}
+	chirp := Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserID:    dbChirp.UserID,
+	}
+	respondWithJSON(w, http.StatusCreated, chirp)
+}
+
+func (cfg *apiConfig) resetUsers(w http.ResponseWriter, r *http.Request) {
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("PLATFORM environment variable is not set")
+		return
+	}
+	if platform == "dev" {
+		err := cfg.dbQueries.ResetUsers(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error resetting users: %v", err)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		log.Println("Users reset successfully")
+		return
+	}
+	w.WriteHeader(http.StatusForbidden)
+	if _, err := w.Write([]byte(http.StatusText(http.StatusForbidden))); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
+
+func (cfg *apiConfig) middlewareServerHitCounter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileServerHits.Add(1)
 		next.ServeHTTP(w, r)
@@ -77,98 +238,62 @@ func (cfg *apiConfig) showMetrics(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, _ *http.Request) {
-	cfg.fileServerHits.Store(0)
+// func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, _ *http.Request) {
+// 	cfg.fileServerHits.Store(0)
+// 	w.WriteHeader(http.StatusOK)
+// }
+
+func healthStatus(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write([]byte("OK")); err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
 }
 
-func chirpValidateHandler(w http.ResponseWriter, r *http.Request) {
-	type Chirp struct {
-		Body string `json:"body"`
-	}
-	type ReturnError struct {
-		Error string `json:"error"`
-	}
-	type ReturnValid struct {
-		Valid bool `json:"valid"`
-	}
-	decoder := json.NewDecoder(r.Body)
-	chirp := Chirp{}
-	err := decoder.Decode(&chirp)
+func respondWithJSON[T any](w http.ResponseWriter, statusCode int, payload T) {
+	data, err := json.Marshal(payload)
 	if err != nil {
-		error := ReturnError{
-			Error: "Something went wrong",
-		}
-		data, err := json.Marshal(error)
-		if err != nil {
-			w.WriteHeader(500)
-			log.Println("Error marshalling JSON")
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(500)
-		if _, err := w.Write(data); err != nil {
-			log.Printf("Error writing response: %v", err)
-		}
+		log.Println("Error marshalling JSON")
+		w.WriteHeader(http.StatusInternalServerError)
 		return
-	} else if len(chirp.Body) > 140 {
-		error := ReturnError{
-			Error: "Chirp is too long",
-		}
-		data, err := json.Marshal(error)
-		if err != nil {
-			w.WriteHeader(500)
-			log.Println("Error marshalling JSON")
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		if _, err := w.Write(data); err != nil {
-			log.Printf("Error writing response: %v", err)
-		}
-		return
-	} else {
-		valid := ReturnValid{
-			Valid: true,
-		}
-		data, err := json.Marshal(valid)
-		if err != nil {
-			w.WriteHeader(500)
-			log.Println("Error marshalling JSON")
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
-		if _, err := w.Write(data); err != nil {
-			log.Printf("Error writing response: %v", err)
-		}
-		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	if _, err := w.Write(data); err != nil {
+		log.Printf("Error writing response: %v", err)
 	}
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 	dbURL := os.Getenv("DB_URL")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
 	dbQueries := database.New(db)
+	cfg := apiConfig{
+		dbQueries: dbQueries,
+	}
 	mux := http.NewServeMux()
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
-	cfg := apiConfig{dbQueries: dbQueries}
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/", cfg.middleware(handler))
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		if _, err := w.Write([]byte("OK")); err != nil {
-			log.Printf("Error writing response: %v", err)
-		}
-	})
+	mux.Handle("/app/", cfg.middlewareServerHitCounter(handler))
+	mux.HandleFunc("GET /api/healthz", healthStatus)
 	mux.HandleFunc("GET /admin/metrics", cfg.showMetrics)
-	mux.HandleFunc("POST /admin/reset", cfg.resetMetrics)
-	mux.HandleFunc("POST /api/validate_chirp", chirpValidateHandler)
-	mux.HandleFunc("POST /api/users", cfg.placeholder)
+	mux.HandleFunc("POST /admin/reset", cfg.resetUsers)
+	mux.HandleFunc("POST /api/chirps", cfg.createChirp)
+	mux.HandleFunc("POST /api/users", cfg.createUser)
+	mux.HandleFunc("GET /api/chirps", cfg.getChirps)
+	mux.HandleFunc("GET /api/chirps/{chirpId}", cfg.getChirp)
+	mux.HandleFunc("POST /api/login", cfg.loginUser)
 	err = srv.ListenAndServe()
 	log.Fatal(err)
 }
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,4 +1,11 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
-VALUES (generate_random_uuid(), now(), now(), $1)
+INSERT INTO users (id, created_at, updated_at, email, hashed_password)
+VALUES (gen_random_uuid(), now(), now(), $1, $2)
 RETURNING *;
+
+-- name: ResetUsers :exec
+DELETE FROM users;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1;
